2f2797b976a818a9e37e57a467c9bc9dce1d8e78
codehenge <codehenge@gmail.com>
2019-01-25 21:23:48 -0800

    Completed most of left side linear (autonomous) mode code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideLinear.java
@@ -17,22 +17,82 @@ public class LeftSideLinear extends EncoderCameraLinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        while(opModeIsActive()) {
+        for(int i=0;i<10;i++) {
+            boolean dummy = opModeIsActive();
             evaluateImage(); //Most deceptively simple code statement in the UNIVERSE.
             if((leftCount > middleCount) && (leftCount > rightCount)) {
                 telemetry.addLine("The left mineral is gold!");
-            }
-            else if((rightCount > middleCount) && (rightCount > leftCount)) {
+                originalReading = 1;
+            } else if((rightCount > middleCount) && (rightCount > leftCount)) {
                 telemetry.addLine("The right mineral is gold!");
-            }
-            else {
+                originalReading = 2;
+            } else {
                 telemetry.addLine("The middle mineral is gold!");
+                originalReading = 3;
             }
-
             telemetry.update();
+        }
+        runtime.reset();
+        while((runtime.time() < 8.6) && opModeIsActive()) {
+            hook.setPower(1.0);
+        }
+        DriveStraight(3f, 0.5f);
+        while((runtime.time() < 1) && opModeIsActive()) {
 
         }
+        hook.setPower(0.0);
 
+        //Left Path
+        if (originalReading == 1) {
+            DriveStraight(6f, 0.6f);
+            EncoderTurn(36, 0.5f);
+            DriveStraight(23f, 1.0f);
+            DriveStraight(-16f, 0.6f);
+            EncoderTurn(45f, 0.5f);
+            DriveStraight(32f, 1.0f);
+            DriveStraight(4.5f, 0.3f);
+            EncoderTurn(63, 0.5f);
+            DriveStraight(54f, 1.0f);
+            elevator.setPower(0.32);
+            runtime.reset();
+            while((runtime.time() < 2) && opModeIsActive());
+            DriveStraight(-90f, 1.0f);
+        }
+        //Right Path
+        else if (originalReading == 2) {
+            DriveStraight(6f, 0.5f);
+            EncoderTurn(-36f, 0.5f);
+            DriveStraight(23f, 1.0f);
+            DriveStraight(-16f, 0.5f);
+            EncoderTurn(122f, 0.5f);
+            DriveStraight(44f, 1.0f);
+            DriveStraight(4.5f, 0.3f);
+            EncoderTurn(57f, 0.5f);
+            DriveStraight(54f, 1.0f);
+            elevator.setPower(0.32);
+            runtime.reset();
+            while((runtime.time() < 2) && opModeIsActive());
+            DriveStraight(-90f, 1.0f);
+        }
+        //Middle Path
+        else if (originalReading == 3 || originalReading == 0) {
+            DriveStraight(22f, 1.0f);
+            DriveStraight(-11f, 0.5f);
+            EncoderTurn(90f, 0.5f);
+            DriveStraight(39f, 1.0f);
+            DriveStraight(9f, 0.3f);
+            EncoderTurn(48, 0.5f);
+            DriveStraight(53f, 1.0f);
+            elevator.setPower(0.32);
+            runtime.reset();
+            while((runtime.time() < 2) && opModeIsActive());
+            DriveStraight(-90f, 1.0f);
+        }
+        runtime.reset();
+        while((runtime.time() < 8.8) && opModeIsActive()) {
+            elevator.setPower(0.05);
+            hook.setPower(-1.0);
+        }
 
     }
 }
